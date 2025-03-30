163f51b96fd4d40744bfb09c91e96be4e3d2ee34
Dafydd Jenkins <dafydd.a.jenkins@gmail.com>
2025-03-30 20:46:20 -0300

    pruebas changes

--- pruebas.r
+++ pruebas.r
@@ -4,3 +4,18 @@ x_train <- data[1:500, 1:3]
 data
 x_train
 round(3/2)
+
+
+lista <- list(1,2,3)
+lista
+lista2 <- list(4,5,6)
+lista2
+gran_lista <- list(lista)
+gran_lista2 <- list(lista2)
+
+gran_lista <- append(gran_lista, gran_lista2)
+gran_lista[1] 
+gran_lista[2]
+gran_lista[3]
+gran_lista[4]
+
